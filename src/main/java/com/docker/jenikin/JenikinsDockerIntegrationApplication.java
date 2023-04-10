f9139fe145da1ad0a47a5c01d8472ecf05cd7492
jenikinsdocker <jenikinsdocker@gmail.com>
2023-04-10 15:45:48 +0530

    first commit

---
+++ src/main/java/com/docker/jenikin/JenikinsDockerIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.docker.jenikin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenikinsDockerIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenikinsDockerIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/docker/jenikin/JenikinsDockerIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docker.jenikin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenikinsDockerIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
